5049a35060f65e4aa7b21682655511cb7e3cc762
kaydurgu <mzhanbolot@gmail.com>
2023-10-04 15:04:16 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Content from "./components/Content";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Exam Marks</h1>
+        <Content></Content>
       </header>
+        <h3>This Page is created as the part of lab works Front end Course</h3>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,25 @@
+import "./Content.css"
+function Content(){
+    return (
+        <div className="Back">
+            <table>
+            <tr>
+                <th>Course</th>
+                <th>Name</th>
+                <th>Mark</th>
+            </tr>
+            <tr>
+                <td>English</td>
+                <td>Turdubaev</td>
+                <td>A+</td>
+            </tr>
+            <tr>
+                <td>English</td>
+                <td>Moldoshev</td>
+                <td>A-</td>
+            </tr>
+            </table>
+        </div>
+    );
+}
+export default Content;
